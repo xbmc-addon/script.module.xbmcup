4602785ba94d53e2f94c5820ea5b6879c0c4aee7
Maxim Khvorov <maxmang@gmail.com>
2016-08-10 22:26:41 +0300

    Remove ControlCheckMark class

--- src/lib/xbmcup/_control.py
+++ src/lib/xbmcup/_control.py
@@ -3,11 +3,10 @@
 __author__ = 'hal9000'
 __all__ = ['Button', 'Check', 'Edit', 'Fade', 'Group', 'Image', 'Label', 'List', 'Progress', 'Radio', 'Slider', 'Text']
 
-from xbmcgui import ControlButton, ControlCheckMark, ControlEdit, ControlFadeLabel, ControlGroup, ControlImage,\
+from xbmcgui import ControlButton, ControlEdit, ControlFadeLabel, ControlGroup, ControlImage,\
     ControlLabel, ControlList, ControlProgress, ControlRadioButton, ControlSlider, ControlTextBox
 
 Button = ControlButton
-Check = ControlCheckMark
 Edit = ControlEdit
 Fade = ControlFadeLabel
 Group = ControlGroup
